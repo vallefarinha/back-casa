822fda9da8d67e86f80faf4c1f638e2a69576649
Gabriela Farinha <vallefarinha@gmail.com>
2024-04-17 10:04:26 -0300

    add migrations and models

---
+++ database/migrations/2024_04_17_125810_create_comments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('content', 1000);
+            $table->string('author');
+            $table->string('email');
+            $table->foreign('post_id')->references('id')->on('posts');
+            $table->unsignedBigInteger('post_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
