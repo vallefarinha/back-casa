e85de4a4cc528a8517d2cbe03e26f766ba3b42f4
NathaRuiz <nruizm98@gmail.com>
2024-04-18 15:18:42 +0200

    refactorice test_list_categories

--- tests/Feature/CategoryControllerTest.php
+++ tests/Feature/CategoryControllerTest.php
@@ -26,16 +26,12 @@ class CategoryControllerTest extends TestCase
 
     public function test_list_categories()
     {
-        $categories = Category::factory()->count(5)->create();
-
         $response = $this->getJson('/api/categories');
         $response->assertStatus(200);
 
         $responseData = $response->json();
         $this->assertArrayHasKey('categories', $responseData, 'The response does not contain categories.');
-
-        $retrievedCategories = $responseData['categories'];
-        $this->assertCount(5, $retrievedCategories, 'The response does not contain the expected number of categories.');
+        $response->assertJsonCount(Category::count());
     }
 
     public function test_show_category()
